5a8f4cdb31442ee45fdd2f369ecc8aa3f48533d7
Lorelai Knutti <virgoknight2@gmail.com>
2022-04-13 11:39:43 -0400

    My best Guesses. I have a lot of studying to do

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-// write your code here
+let json = new Request(`http://localhost:3000/ramens`)
+fetch(json)
+ .then(response => respoonse.json())
+ .then(ramen= () =>{
+      let ramenDietails=JSON.image
+ return  document.getItemById(`ramen-menue`).append(ramenDietails)
+ })
+ramen(json)
+    
+const formData = document.querySelector(`form`)
+function appendFormInfo(){
+
+}
+console.log (formData);
